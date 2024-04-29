9566359c3ea373bcca9b9d728f617a58ab4ddd4d
nbz4 <nbz4@akr.com>
2024-04-29 16:26:01 +0530

    add new file

---
+++ app.js
@@ -0,0 +1,23 @@
+// 'use strict';
+
+// const express=require('express');
+// const config=require('./config');
+// const cors=require('cors');
+// const bodyParser=require('body-parser');
+// const eventRoutes=require('./routes/eventRoutes');
+
+
+// //const eventRoutes=require('./routes/eventRoutes');
+// const app=express();
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use('/api',eventRoutes.routes);
+// app.get('/', (req, res) => {
+//     return res.status(234).send('Admin')
+// })
+// //app.use('/api,eventRoutes.routes');
+
+
+
+
+console.log("hai y r u accting like this")
